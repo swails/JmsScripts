600355260e48ebdcbc32677f9380ac0d3c382130
Jason Swails <jason.swails@gmail.com>
2010-08-03 11:00:22 -0400

    Change natom.py from a soft link to a hard link, as it probably should be.

--- natom.py
+++ natom.py
@@ -1,1 +1,22 @@
-nres.py
+#!/usr/bin/env python
+
+# Find the number of residues in a given prmtop
+
+from sys import stderr, stdout, exit, argv
+from readparm import amberParm
+from os import path
+
+def usage():
+   print >> stderr, "{0} <prmtop>".format(argv[0])
+   exit()
+
+if len(argv) != 2:
+   usage()
+
+x = amberParm(argv[1])
+
+if x.exists:
+   print x.ptr(path.split(argv[0])[1].strip('.py'))
+
+else:
+   print >> stderr, '{0} does not exist!'.format(argv[1])

e35b663519957595eb1e0442d9055dccdb0618b7
Benjamin P. Roberts <roberts@qtp.ufl.edu>
2010-08-27 11:44:49 -0400

    Fixed up object handling (JS, BPR)

--- mdin.py
+++ mdin.py
@@ -2,29 +2,33 @@
 
 class mdin:
 
-   import cntrl, ewald
+   from cntrl import cntrl
+   from ewald import ewald
    from sys import stderr, stdout
 
    # Variable list: These are dictionaries corresponding to the different
    # namelists that can be specified. They will be defined in __init__
 
-   # cntrl
-   # cntrl_defaults
-   # ewald
-   # ewald_defaults
-
-   program = ''
+   cntrl_obj = cntrl
+   cntrl_obj_defaults = cntrl
+   ewald_obj = ewald
+   ewald_obj_defaults = ewald
+   
+   cntrl_nml = {}
+   ewald_nml = {}
+   cntrl_nml_defaults = {}
+   ewald_nml_defaults = {}
 
    def __init__(self, program = 'sander'):
       if program == "sander":
-         self.cntrl = cntrl.cntrl.sander
-         self.cntrl_defaults = cntrl.cntrl.sander
-         self.ewald = ewald.ewald.sander
-         self.ewald_defaults = ewald.ewald.sander
+         self.cntrl_nml = cntrl_obj.sander
+         self.cntrl_nml_defaults = cntrl_obj_defaults.sander
+         self.ewald_nml = ewald.
+         self.ewald_nml_defaults = ewald_obj_defaults.sander
       elif program == "pmemd":
-         self.cntrl = cntrl.cntrl.pmemd
-         self.cntrl_defaults = cntrl.cntrl.pmemd
-         self.ewald = ewald.ewald.pmemd
-         self.ewald_defaults = ewald.ewald.pmemd
+         self.cntrl = cntrl_obj.pmemd
+         self.cntrl_defaults = cntrl_obj_defaults.pmemd
+         self.ewald = ewald_obj.pmemd
+         self.ewald_defaults = ewald_obj_defaults.pmemd
       else:
          print >> stderr, 'Error: program (%s) unrecognized!' % program

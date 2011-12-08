e3d443aa4f789f1e85e92584dd34280fd6044c84
Jason Swails <jason.swails@gmail.com>
2011-12-08 13:02:37 -0500

    Add a test for phipsigen.py

--- Tests/Test.sh
+++ Tests/Test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+test_cleanup() {
+if [ $1 -ne 0 ]; then
+   echo "Failed. See $2"
+else
+   echo "PASSED"
+   /bin/rm -f $2
+fi
+}
+   
 # This script tests all of the scripts in ScriptDevel that actually have tests
 # associated with them here.
 
@@ -10,39 +19,45 @@ printf "   testing minimization output:   "
 
 python ../mdout.py --mdout trpcage.nowat.min.mdout > tmp
 diff mdout_trpcage_min.check tmp > mdout_trpcage_min.diff 2>&1
-
-if [ $? -ne 0 ]; then
-   echo "Failed. See mdout_trpcage_min.diff"
-else
-   echo "PASSED"
-   /bin/rm -f mdout_trpcage_min.diff
-fi
+test_cleanup $? mdout_trpcage_min.diff
 
 printf "   testing heating output:        "
 
 python ../mdout.py --mdout trpcage.nowat.heat0.mdout > tmp
 diff mdout_trpcage_heat.check tmp > mdout_trpcage_heat.diff 2>&1
-
-if [ $? -ne 0 ]; then
-   echo "Failed. See mdout_trpcage_heat.diff"
-else
-   echo "PASSED"
-   /bin/rm -f mdout_trpcage_heat.diff
-fi
+test_cleanup $? mdout_trpcage_heat.diff
 
 printf "   testing combining mdout files: "
 
 python ../mdout.py --mdout trpcage.nowat.heat0.mdout \
                    --next-mdout trpcage.nowat.heat1.mdout > tmp
 diff mdout_trpcage_combine.check tmp > mdout_trpcage_combine.diff 2>&1
+test_cleanup $? mdout_trpcage_combine.diff
 
-if [ $? -ne 0 ]; then
-   echo "Failed. See mdout_trpcage_combine.diff"
-else
-   echo "PASSED"
-   /bin/rm -f mdout_trpcage_combine.diff
-fi
+echo "============================================================"
+echo "Testing phipsigen.py"
 
+../phipsigen.py -i phipsi.in -o rama \
+                -p acfca.neutral.state00.parm7 \
+                -y acfca.neutral.nowat.md.nc > /dev/null
+
+printf "   Checking residue 2 data:    "
+diff rama.2.dat.check rama.2.dat > rama.2.dat.diff
+test_cleanup $? rama.2.dat.diff
+
+printf "   Checking residue 3 data:    "
+diff rama.3.dat.check rama.3.dat > rama.3.dat.diff
+test_cleanup $? rama.3.dat.diff
+
+printf "   Checking residue 2 binning: "
+diff rama.2.bin.dat.check rama.2.bin.dat > rama.2.bin.diff
+test_cleanup $? rama.2.bin.diff
+
+printf "   Checking residue 3 binning: "
+diff rama.3.bin.dat.check rama.3.bin.dat > rama.3.bin.diff
+test_cleanup $? rama.3.bin.diff
+
+/bin/rm -f _PHIPSI_* rama.?.gnu rama.?.dat rama.?.bin.dat
 echo "============================================================"
 
 /bin/rm -f tmp

45577b5e1e38f9c4cfbb832aeb65e33b1f3246ba
Jason Swails <jason.swails@gmail.com>
2012-12-03 17:49:50 -0500

    Update the local amber testing script to automagically build amber at the beginning before running the tests.

--- TestAmberLocal.sh
+++ TestAmberLocal.sh
@@ -4,12 +4,25 @@
 #PBS -o LocalTest.out
 #PBS -e LocalTest.err
 
-cd $AMBERHOME
-
 unset DO_PARALLEL TESTsander
 
-/bin/rm -fr logs/
+# First, update my code
+cd $AMBERHOME
+git pull 2>&1 > /dev/null
+git clean -f -x -d 2>&1 > /dev/null
 
+echo "Configuring serial at `date`"
+(./configure intel 2>&1) > $HOME/TestLogs/`date +%m+%d+%y`_serial_config.log
+echo "Making serial at `date`"
+(make -j6 install 2>&1) > $HOME/TestLogs/`date +%m+%d+%y`_serial_make.log
+
+echo "Configuring parallel at `date`"
+(./configure -mpi intel 2>&1) > $HOME/TestLogs/`date +%m+%d+%y`_parallel_config.log || \
+                              error "Configuring parallel"
+echo "Making parallel at `date`"
+(make -j6 install 2>&1) > $HOME/TestLogs/`date +%m+%d+%y`_parallel_make.log
+
+# Now test.
 echo "Beginning serial tests at `date`"
 
 (make test.serial 2>&1) > $HOME/TestLogs/`date +%m+%d+%y`_serial_test.log

cad0384737e2c40dcc1ad4f395d03836ef6c6e2a
Jason Swails <jason.swails@gmail.com>
2013-06-06 11:36:38 -0400

    Have amber email stats also get the number of CUDA-related emails.

--- get_amber_email_stats.py
+++ get_amber_email_stats.py
@@ -6,6 +6,8 @@ from optparse import OptionParser
 from urllib import urlopen
 
 mailre = re.compile(r'.*\(([SMTWF][a-z]{2}) *([FMJASOND][a-z]{2}) *(\d+) *(\d{4}).*\)')
+cudare = re.compile(r'(cuda|gpu|nvidia)', re.I)
+cudacount = 0
 count = 0
 
 parser = OptionParser()
@@ -19,48 +21,59 @@ allmails = urlopen('http://archive.ambermd.org/all/')
 print 'Calculating traffic stats for amber mailing list'
 
 yr_mail_cnt = {}
+cuyr_mail_cnt = {}
 mnth_mail_cnt = {}
+cumnth_mail_cnt = {}
 day_mail_cnt = {}
+cuday_mail_cnt = {}
 
 for line in allmails:
    rematch = mailre.match(line)
+   cuadd = int(bool(cudare.findall(line)))
    if rematch:
       day, month, nday, year = rematch.groups()
       try:
          yr_mail_cnt[int(year)] += 1
+         cuyr_mail_cnt[int(year)] += cuadd
       except KeyError:
          yr_mail_cnt[int(year)] = 1
+         cuyr_mail_cnt[int(year)] = cuadd
       try:
          mnth_mail_cnt[month] += 1
+         cumnth_mail_cnt[month] += cuadd
       except KeyError:
          mnth_mail_cnt[month] = 1
+         cumnth_mail_cnt[month] = cuadd
       try:
          day_mail_cnt[day] += 1
+         cuday_mail_cnt[day] += cuadd
       except KeyError:
          day_mail_cnt[day] = 1
+         cuday_mail_cnt[day] = cuadd
+
    if opt.tag is not None and opt.tag in line.lower():
       count += 1
 
 print 'Done calculating traffic stats for mailing list:'
 print ''
 print '     Year |   # Emails'
-print '------------------------'
+print '------------------------' + '-'*15
 keys = yr_mail_cnt.keys()
 keys.sort()
 for key in yr_mail_cnt:
-   print '%9s | %10d' % (key, yr_mail_cnt[key])
+   print '%9s | %10d (%7d CUDA)' % (key, yr_mail_cnt[key], cuyr_mail_cnt[key])
 
 print '\n------------------------\n'
 print '    Month |   # Emails'
-print '------------------------'
+print '------------------------' + '-'*15
 for key in mnth_mail_cnt:
-   print '%9s | %10d' % (key, mnth_mail_cnt[key])
+   print '%9s | %10d (%7d CUDA)' % (key, mnth_mail_cnt[key], cumnth_mail_cnt[key])
 
 print '\n------------------------\n'
 print '      Day |   # Emails'
-print '------------------------'
+print '------------------------' + '-'*15
 for key in day_mail_cnt:
-   print '%9s | %10d' % (key, day_mail_cnt[key])
+   print '%9s | %10d (%7d CUDA)' % (key, day_mail_cnt[key], cuday_mail_cnt[key])
 
 if opt.name is not None:
    print '\n'

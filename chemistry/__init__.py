06302793dd14f8495ba9dae988a7232460314118
Jason Swails <jason.swails@gmail.com>
2013-09-18 12:42:43 -0400

    Discard the source code present in the Amber repository. It's becoming a pain to keep track of in so many places.

--- chemistry/__init__.py
+++
@@ -1,9 +0,0 @@
-""" The chemistry package manipulates molecular structures and provides
-    a way to go between standard and amber file formats, manipulate
-    structures, etc. """
-
-__all__ = ['molecule','formats','periodic_table','exceptions','amber']
-
-__version__ = '13.0'
-__author__ = 'Jason Swails'
-
--- chemistry/amber/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Package that provides an API to Amber-specific files """
-
-__version__ = "1.0"
-__author__ = "Jason Swails <jason.swails@gmail.com>"
-
-__all__ = ['readparm', 'leaprc', 'residue', 'mask']
-
